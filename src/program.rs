5d3b2b6ca45ceda2c374e6866e471f2774d4a666
Curtis McEnroe <programble@gmail.com>
2015-08-02 16:55:59 -0400

    Implement program commands

--- src/program.rs
+++ src/program.rs
@@ -1,7 +1,9 @@
 //! Program space and execution.
 
+use std::io;
+use std::io::Read;
 use consts;
-use pc::{Pc, Direction};
+use pc::Pc;
 
 /// Program space, counter and stack.
 pub struct Program {
@@ -20,5 +22,159 @@ impl Program {
             strmode: false,
             stack: Vec::new(),
         }
+    }
+
+    /// Executes the next command, returning true if execution should continue.
+    pub fn step(&mut self) -> bool {
+        match self.data[self.pc.x][self.pc.y] {
+
+            b'"'              => self.toggle_strmode(),
+            b if self.strmode => self.push(b),
+
+            b'+' => self.add(),
+            b'-' => self.subtract(),
+            b'*' => self.multiply(),
+            b'/' => self.divide(),
+            b'%' => self.modulo(),
+
+            b'!' => self.not(),
+            b'`' => self.greater(),
+
+            b'>' => self.pc.right(),
+            b'<' => self.pc.left(),
+            b'^' => self.pc.up(),
+            b'v' => self.pc.down(),
+            b'?' => self.pc.random(),
+
+            b'_' => self.horizontal_if(),
+            b'|' => self.vertical_if(),
+
+            b':'  => self.dup(),
+            b'\\' => self.swap(),
+            b'$'  => { self.stack.pop(); },
+
+            b'.' => self.output_value(),
+            b',' => self.output_ascii(),
+
+            b'#' => self.pc.step(),
+
+            b'g' => self.get(),
+            b'p' => self.put(),
+
+            b'&' => self.input_value(),
+            b'~' => self.input_ascii(),
+
+            b'@' => return false,
+
+            _ => (),
+        }
+
+        self.pc.step();
+
+        true
+    }
+
+    fn pop(&mut self) -> u8 {
+        self.stack.pop().unwrap_or(0)
+    }
+
+    fn push(&mut self, b: u8) {
+        self.stack.push(b);
+    }
+
+    fn toggle_strmode(&mut self) {
+        self.strmode = !self.strmode;
+    }
+
+    fn add(&mut self) {
+        let s = self.pop() + self.pop();
+        self.push(s);
+    }
+
+    fn subtract(&mut self) {
+        let (b, a) = (self.pop(), self.pop());
+        self.push(a - b);
+    }
+
+    fn multiply(&mut self) {
+        let p = self.pop() * self.pop();
+        self.push(p);
+    }
+
+    fn divide(&mut self) {
+        let (b, a) = (self.pop(), self.pop());
+        self.push(a / b);
+    }
+
+    fn modulo(&mut self) {
+        let (b, a) = (self.pop(), self.pop());
+        self.push(a % b);
+    }
+
+    fn not(&mut self) {
+        let t = self.pop();
+        self.push(if t == 0 { 1 } else { 0 });
+    }
+
+    fn greater(&mut self) {
+        let (b, a) = (self.pop(), self.pop());
+        self.push(if a > b { 1 } else { 0 });
+    }
+
+    fn horizontal_if(&mut self) {
+        if self.pop() == 0 {
+            self.pc.right();
+        } else {
+            self.pc.left();
+        }
+    }
+
+    fn vertical_if(&mut self) {
+        if self.pop() == 0 {
+            self.pc.down();
+        } else {
+            self.pc.up();
+        }
+    }
+
+    fn dup(&mut self) {
+        let t = self.stack.last().cloned().unwrap_or(0);
+        self.push(t);
+    }
+
+    fn swap(&mut self) {
+        let (a, b) = (self.pop(), self.pop());
+        self.push(a);
+        self.push(b);
+    }
+
+    fn output_value(&mut self) {
+        print!("{} ", self.pop());
+    }
+
+    fn output_ascii(&mut self) {
+        print!("{}", self.pop() as char);
+    }
+
+    fn get(&mut self) {
+        let (y, x) = (self.pop(), self.pop());
+        let v = self.data[x as usize][y as usize];
+        self.push(v);
+    }
+
+    fn put(&mut self) {
+        let (y, x, v) = (self.pop(), self.pop(), self.pop());
+        self.data[x as usize][y as usize] = v;
+    }
+
+    fn input_value(&mut self) {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        self.push(buf.parse().unwrap_or(0));
+    }
+
+    fn input_ascii(&mut self) {
+        let v = io::stdin().bytes().next().unwrap().unwrap();
+        self.push(v);
     }
 }
