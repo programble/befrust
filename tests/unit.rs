695419a86734265ed66fdc21e21b67b05374efcc
Curtis McEnroe <programble@gmail.com>
2015-08-04 01:47:55 -0400

    Add push unit tests

--- tests/unit.rs
+++ tests/unit.rs
@@ -35,3 +35,22 @@ fn right() {
 fn random() {
     test_io(b"?.@.<\n>   ^", &[], b"0 ");
 }
+
+#[test]
+fn push() {
+    test_io(b"0.@", &[], b"0 ");
+    test_io(b"1.@", &[], b"1 ");
+    test_io(b"2.@", &[], b"2 ");
+    test_io(b"3.@", &[], b"3 ");
+    test_io(b"4.@", &[], b"4 ");
+    test_io(b"5.@", &[], b"5 ");
+    test_io(b"6.@", &[], b"6 ");
+    test_io(b"7.@", &[], b"7 ");
+    test_io(b"8.@", &[], b"8 ");
+    test_io(b"9.@", &[], b"9 ");
+}
+
+#[test]
+fn strmode() {
+    test_io(b"\"@\".@", &[], b"64 ");
+}
