f257d97d68a69ea0afacd377fc4f05a2c9082b1d
Scott Olson <scott@solson.me>
2015-08-04 19:11:49 -0400

    Loosen Program's input constraint to Read and internally wrap with BufReader.

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -1,18 +1,16 @@
 extern crate befrust;
 extern crate timebomb;
 
-use std::io;
 use self::timebomb::timeout_ms;
 use self::befrust::program::Program;
 
 pub fn test_io(source: &'static [u8], input: &'static [u8], expected: &'static [u8]) {
     timeout_ms(move || {
 
-        let mut inp = io::BufReader::new(input);
         let mut out = Vec::<u8>::new();
 
         {
-            let mut p = Program::new(&mut inp, &mut out);
+            let mut p = Program::new(input, &mut out);
             p.load(source);
             p.run();
         }
