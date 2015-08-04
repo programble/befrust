60ddcb8026326862b21bd99e4844994b3358dceb
Curtis McEnroe <programble@gmail.com>
2015-08-04 00:08:20 -0400

    Add input and timeouts to integration tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,21 +1,28 @@
 extern crate befrust;
+extern crate timebomb;
 
 use std::io;
+use timebomb::timeout_ms;
 use befrust::program::Program;
 
-fn test_output(source: &[u8], expected: &[u8]) {
-    let mut out = Vec::<u8>::new();
+fn test_io(source: &'static [u8], input: &'static [u8], expected: &'static [u8]) {
+    timeout_ms(move || {
 
-    {
-        let mut p = Program::new(io::empty(), &mut out);
-        p.load(source);
-        p.run();
-    }
+        let mut inp = io::BufReader::new(input);
+        let mut out = Vec::<u8>::new();
 
-    assert_eq!(out, expected);
+        {
+            let mut p = Program::new(&mut inp, &mut out);
+            p.load(source);
+            p.run();
+        }
+
+        assert_eq!(out, expected);
+
+    }, 1000);
 }
 
 #[test]
 fn hello_world() {
-    test_output(b"\"!dlroW ,olleH\">:#,_@", b"Hello, World!");
+    test_io(b"\"!dlroW ,olleH\">:#,_@", &[], b"Hello, World!");
 }
